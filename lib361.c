22c2ef3b1b5d80fefe0d2d33181d63070a1e1bbc
Miss  Tank <tankankita02@gmail.com>
2016-12-18 18:17:11 +0530

    Add files via upload

---
+++ lib361.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	int secretoperation(int x, int y, int z){
+        	if (x==3 || y ==6)return 10;
+        	else if(x==1 || y==2)return 4;
+	}
+
+
+int main(void){
+return 0;
+}
